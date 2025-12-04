0212f0961c6e2698cfd4c4b025cef79ddd3a694e
Aayush_Raj <98972400+aprogramme@users.noreply.github.com>
2025-12-04 17:02:21 +0530

    Add files via upload

---
+++ 3. JavaScript/Day 31 (Projects)/03. Love Calculator/script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();  // prevent from refreshing the page
+
+    const Boy = document.getElementById("boy");
+    const Girl = document.getElementById("girl");
+
+    // catch the value enter by user
+    const v1 = Boy.value.length; 
+    const v2 = Girl.value.length;
+
+    // const result = Math.pow(v1+v2, 2)%101;
+    const result = Math.pow(v1*v2, 2)%101;
+
+    const res = document.querySelector('h2');
+    res.textContent = `Result : ${result}%`;
+
+    form.reset();  // reset the value entered in form
+
+})
